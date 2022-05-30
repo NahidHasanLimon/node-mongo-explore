5fd84632d470427127f536daafca583a4dd91be2
jatrinahidhasanlimon <nhlimon.jatri@gmail.com>
2022-05-30 17:00:22 +0600

    add book endpoint added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const {ObjectId} = require('mongodb')
 const {connectToDb, getDb} = require('./db')
+var bodyParser = require('body-parser');
+
 // init 
 const app = express()
+app.use(bodyParser.json());
 // database connection 
 let db 
 connectToDb( (err) =>{
@@ -31,7 +34,7 @@ app.get('/books/:id',(req, res)=>{
     if(!ObjectId.isValid(req.params.id)){
         res.status(500).json({mssg: 'Invalid ID'})
     }
-    data = db.collection('books').findOne({_id: ObjectId(req.params.id)}).then( doc =>{
+    db.collection('books').findOne({_id: ObjectId(req.params.id)}).then( doc =>{
        if(doc == null){
         res.status(404).json('Document Not Found')
        }
@@ -39,4 +42,18 @@ app.get('/books/:id',(req, res)=>{
     }).catch( ()=> {
          res.status(500).json({mssg: 'Failed top fetch'})
     })
+
+
+})
+app.post('/books', (req,res)=>{
+    const book = req.body
+    // res.json(book)
+    db.collection('books').insertOne(book)
+    .then( result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({err: 'Failed to store'})
+    })
+
 })
