ccb5459e27a60cbc5090377fbdf5318b110602ac
jatrinahidhasanlimon <nhlimon.jatri@gmail.com>
2022-05-30 17:39:34 +0600

    update, delete added

--- app.js
+++ app.js
@@ -57,3 +57,30 @@ app.post('/books', (req,res)=>{
     })
 
 })
+app.delete('/books/:id',(req, res)=>{
+    if(!ObjectId.isValid(req.params.id)){
+        res.status(500).json({mssg: 'Invalid ID'})
+    }
+    db.collection('books').deleteOne({_id: ObjectId(req.params.id)}).then( doc =>{
+       if(doc == null){
+        res.status(404).json('Document Not Found')
+       }
+       res.status(200).json(doc)
+    }).catch( ()=> {
+         res.status(500).json({mssg: 'Failed top delete'})
+    })
+})
+app.patch('/books/:id',(req, res)=>{
+    const book = req.body
+    if(!ObjectId.isValid(req.params.id)){
+        res.status(500).json({mssg: 'Invalid ID'})
+    }
+    db.collection('books').updateOne({_id: ObjectId(req.params.id)}, {$set: book } ).then( result =>{
+       if(result == null){
+        res.status(404).json('Document Not Found')
+       }
+       res.status(200).json(result)
+    }).catch( ()=> {
+         res.status(500).json({mssg: 'Update Failed'})
+    })
+})
